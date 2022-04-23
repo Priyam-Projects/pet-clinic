ab24c5a3272bfdd73ebfb01a6e18cd2ea11f3ba5
Priyam raj <priyamraj@Priyams-MacBook-Air.local>
2022-04-23 18:53:11 +0530

    start

---
+++ src/main/java/com/priyamprojects/petclinicpriyam/PetClinicPriyamApplication.java
@@ -0,0 +1,13 @@
+package com.priyamprojects.petclinicpriyam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicPriyamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicPriyamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/priyamprojects/petclinicpriyam/PetClinicPriyamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyamprojects.petclinicpriyam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicPriyamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
